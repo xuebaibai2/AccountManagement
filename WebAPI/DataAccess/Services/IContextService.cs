655d4d21501767f06a7d462c2ba93179dba9637f
Cayden Li <xuebaibai2@hotmail.com>
2018-11-26 17:10:02 +1100

    Add CRUD User functions on UserService
    created UsersController to handle web api call

---
+++ WebAPI/DataAccess/Services/IContextService.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace WebAPI.DataAccess.Services
+{
+    interface IContextService<T>
+    {
+        IEnumerable<T> GetList();
+        Task<T> Get(int id);
+        Task<T> Post(T entity);
+        Task<T> Put(T entity);
+        Task<T> Delete(int id);
+    }
+}
